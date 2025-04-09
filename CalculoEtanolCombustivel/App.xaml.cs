2a7953538a38d9802f6a02c3bf3d67a7e4212b0e
Leonardo Gonçalves Cordeiro Cruz Filho <leonardogccf@gmail.com>
2025-04-09 18:24:06 -0300

    first commit

---
+++ CalculoEtanolCombustivel/App.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace CalculoEtanolCombustivel
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            return new Window(new AppShell());
+        }
+    }
+}
---
+++ CalculoEtanolCombustivel/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace CalculoEtanolCombustivel
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
